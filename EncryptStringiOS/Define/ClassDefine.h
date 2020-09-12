1c70f0f58d3da5def8e4b5fdbb186580fbe2bce0
Silence <374619540@qq.com>
2020-09-12 21:57:53 +0800

    workspace

--- EncryptStringiOS/Define/ClassDefine.h
+++ EncryptStringiOS/Define/ClassDefine.h
@@ -1,2 +1,2 @@
-#define LoginCheck poTxxyZxtZPxkx //LoginCheck
-#define httpPostUserInfo xJxUxUpQOqnLOe //httpPostUserInfo
+#define LoginCheck bfxAxvAIxUxxux //LoginCheck
+#define httpPostUserInfo xqxNbxxxxxxxlB //httpPostUserInfo
